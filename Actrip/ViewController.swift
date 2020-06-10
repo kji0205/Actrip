1bc8e133cef1b48ed36d7691ef6591565063751c
Jimmy <dogspaw8@gmail.com>
2020-06-10 21:07:38 +0900

    Initial Commit

---
+++ Actrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actrip
+//
+//  Created by Jimmy on 2020/06/10.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
