4dd8fdd28f72d7e8bfd2987d1c80586bfafffa98
Jimmy <dogspaw8@gmail.com>
2020-08-12 07:12:19 +0900

    .

---
+++ Actrip/NoticeViewController.swift
@@ -0,0 +1,68 @@
+//
+//  NoticeViewController.swift
+//  Actrip
+//
+//  Created by Jimmy on 2020/07/25.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class NoticeViewController: ATControllerBase, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        guard let url = URL(string:"https://actrip.co.kr/notice") else {return}
+//
+//        let request = URLRequest(url: url)
+//
+//        webView?.load(request)
+        
+        spinner.hidesWhenStopped = true
+        
+        var url: URL?
+        url = URL(string: "https://actrip.co.kr/notice")
+        let request = URLRequest(url: url!)
+
+        webView.navigationDelegate = self // set the delegate
+
+        webView.load(request)
+        
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        webView.isHidden = true
+        spinner.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        print("Finished navigating to url \(String(describing: webView.url))");
+
+        let script="(function(){ var footer=document.getElementById('headerWrap'); footer.remove(); }())"
+
+        webView.evaluateJavaScript(script) { (result, error) in
+            if error != nil {
+                print(result ?? "")
+            }
+            webView.isHidden = false
+            self.spinner.stopAnimating()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
